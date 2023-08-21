3667bd212e2aa5ff34bbfc7a51cb8b6cb1496a89
Syed Aman <syednashikaman@gmail.com>
2023-08-21 11:49:37 -0400

    unit test

--- test_apm.py
+++ test_apm.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import MagicMock
 from apm_tracker import ApmTracker
 
+
 class TestApmTracker(unittest.TestCase):
 
     def setUp(self):
@@ -15,9 +16,25 @@ class TestApmTracker(unittest.TestCase):
         current_APM = self.apm_tracker.keystrokes + self.apm_tracker.mouse_clicks
         current_EAPM = self.apm_tracker.effective_actions
 
+        peak_APM = max(self.apm_tracker.APM_list + [current_APM])  # Use max to safely handle empty lists
+        peak_EAPM = max(self.apm_tracker.EAPM_list + [current_EAPM])
+
+        average_APM = (self.apm_tracker.cumulative_actions + current_APM) / (
+                    self.apm_tracker.intervals_since_start + 1) if self.apm_tracker.intervals_since_start != -1 else current_APM
+        average_EAPM = (self.apm_tracker.cumulative_effective_actions + current_EAPM) / (
+                    self.apm_tracker.intervals_since_start + 1) if self.apm_tracker.intervals_since_start != -1 else current_EAPM
+
         # Assert that both APM and EAPM are 1 for a single keypress
         self.assertEqual(current_APM, 1)
         self.assertEqual(current_EAPM, 1)
+
+        # Assert peak values
+        self.assertEqual(peak_APM, 1)
+        self.assertEqual(peak_EAPM, 1)
+
+        # Assert average values
+        self.assertEqual(average_APM, 1)
+        self.assertEqual(average_EAPM, 1)
 
 
 if __name__ == "__main__":
